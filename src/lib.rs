f03742a8a80d51c3b0026daf1183347fbd07d6e0
makai <mykaelalvesnogueira1@gmail.com>
2025-03-08 12:43:36 -0300

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,2 @@
+pub use proarc_database::*;
+pub use proarc_file_transfer::*;
---
+++ src/main.rs
@@ -0,0 +1,7 @@
+use std::env;
+use std::error::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    Ok(())
+}
