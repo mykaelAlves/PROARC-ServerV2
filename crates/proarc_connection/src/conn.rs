d9825552f527405fe3b4c58704d3c37fd03f14a2
makai <mykaelalvesnogueira1@gmail.com>
2025-03-10 22:58:25 -0300

    testing echo server

--- crates/proarc_connection/src/conn.rs
+++ crates/proarc_connection/src/conn.rs
@@ -1,13 +1,33 @@
 use std::env;
-use tokio::net::TcpListener;
+use tokio::net::{TcpListener, TcpStream};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
 pub async fn listen(addr: String) {
     let listener = TcpListener::bind(addr).await.unwrap();
 
     loop {
-        match listener.accept().await {
-            Ok((_socket, addr)) => println!("new client: {addr:?}"),
-            Err(e) => println!("couldn't get client: {e:?}"),
-        }
+        let (mut socket, _) = listener.accept().await.unwrap();
+
+        tokio::spawn(async move {
+            let mut buf = vec![0; 1024];
+
+            loop {
+                let n = socket
+                    .read(&mut buf)
+                    .await
+                    .expect("failed to read data from socket");
+
+                if n == 0 {
+                    return;
+                }
+
+                println!("Received: {}", String::from_utf8_lossy(&buf[0..n]));
+
+                socket
+                    .write_all(&buf[0..n])
+                    .await
+                    .expect("failed to write data to socket");
+            }
+        });
     }
 }
--- tests/try_conn.py
+++ tests/try_conn.py
@@ -1,5 +1,10 @@
 import socket
 
 clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print("Socket created")
 clientsocket.connect(('127.0.0.1', 9999))
+print("Socket connected")
 clientsocket.send(b'hello')
+print("Message sent")
+msg = clientsocket.recv(1024)
+print("Message received: " + msg.decode())
